76f27b67ff25618cfa6f2babfe01282a7798b7ee
Mao <code@mao.com>
2020-03-31 10:39:34 +0800

    fix checksum

--- Formula/macism.rb
+++ Formula/macism.rb
@@ -3,7 +3,7 @@ class Macism < Formula
   desc "MacOS Input Source Manager"
   homepage "https://github.com/laishulu/macism"
   url "https://github.com/laishulu/macism/releases/download/#{version}/macism-#{version}-x86_64-apple-darwin.tar.gz"
-  sha256 "7f5a47e64906ee3f5be06f9c98572f1e2481d922657e49455cb4f2842d059ef3"
+  sha256 "cf3c5d84d7187272e017f06b06cc18b30b590393138b05ae11a9d83662e8fd5d"
 
   def install
     bin.install "macsim"
