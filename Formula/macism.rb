da83b4c0864b3b9534d50c260602a92710fb8e01
Mao <code@mao.com>
2020-04-21 18:24:39 +0800

    update hash

--- Formula/macism.rb
+++ Formula/macism.rb
@@ -3,7 +3,7 @@ class Macism < Formula
   desc "MacOS Input Source Manager"
   homepage "https://github.com/laishulu/macism"
   url "https://github.com/laishulu/macism/archive/1.0.0.zip"
-  sha256 "3a2fec884705f7fdca6231696f9a0819a5107f7bb0112353c86c93604189ab63"
+  sha256 "1648afd70617e32dcf0ff06e0bd2d05135b66b2b0ed835adaae7c242f3c6372b"
   head "https://github.com/laishulu/macism/archive/master.zip"
 
   def install
