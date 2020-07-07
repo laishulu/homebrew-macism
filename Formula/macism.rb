1a5f9638c1fa5287e24aa2dff04aaa81b336a2cc
Mao <code@mao.com>
2020-07-07 20:38:10 +0800

    bump version

--- Formula/macism.rb
+++ Formula/macism.rb
@@ -1,9 +1,9 @@
 class Macism < Formula
   desc "MacOS Input Source Manager"
   homepage "https://github.com/laishulu/macism"
-  url "https://github.com/laishulu/macism/archive/v1.1.0.tar.gz"
-  version "1.1.0"
-  sha256 "f22391bdc0fe17ad57da46b0610a0fe7c491f128d59c8f4244b778a9bd6d6125"
+  url "https://github.com/laishulu/macism/archive/v1.1.1.tar.gz"
+  version "1.1.1"
+  sha256 "a3adac3ee0e9c1d62d8b10fa1a5d51a31aa98a9189b11e656578f320b72b2e33"
   head "https://github.com/laishulu/macism/archive/master.zip"
 
   def install
