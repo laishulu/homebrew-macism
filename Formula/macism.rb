3eef3f63891d5cd2f761869d3f45ff227666fb02
Mao <code@mao.com>
2020-07-08 17:12:51 +0800

    pump version

--- Formula/macism.rb
+++ Formula/macism.rb
@@ -1,9 +1,9 @@
 class Macism < Formula
   desc "MacOS Input Source Manager"
   homepage "https://github.com/laishulu/macism"
-  url "https://github.com/laishulu/macism/archive/v1.1.1.tar.gz"
-  version "1.1.1"
-  sha256 "a3adac3ee0e9c1d62d8b10fa1a5d51a31aa98a9189b11e656578f320b72b2e33"
+  url "https://github.com/laishulu/macism/archive/v1.2.0.tar.gz"
+  version "1.2.0"
+  sha256 "73b4993a21fc3468343415e99d69f888eae2139c08a528d4925990969accb7a9"
   head "https://github.com/laishulu/macism/archive/master.zip"
 
   def install
