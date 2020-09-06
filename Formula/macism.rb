34d716e992bead3dbd240b8403107dbb89940b9f
Mao <code@mao.to>
2020-09-06 11:46:30 +0800

    revert the last commit

--- Formula/macism.rb
+++ Formula/macism.rb
@@ -1,6 +1,7 @@
 class Macism < Formula
   desc "MacOS Input Source Manager"
   homepage "https://github.com/laishulu/macism"
+  version "1.3.0"
   url "https://github.com/laishulu/macism/archive/v1.3.0.tar.gz"
   sha256 "a41b3acb305dcf4682a9d9beeffe63d01573bb5c97c49f3819132d275ac2bdb9"
   head "https://github.com/laishulu/macism/archive/master.zip"
